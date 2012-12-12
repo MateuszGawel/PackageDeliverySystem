5617cbca27f2f7d92fd357aed260a0d7f929050a
mchrzaszcz <m.s.chrzaszcz@gmail.com>
2012-12-12 18:55:12 +0100

    Puste klasy main + klasa testująca Tester i TesterGUI

---
+++ src/Tester.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: mateusz
+ * Date: 12.12.12
+ * Time: 18:51
+ * To change this template use File | Settings | File Templates.
+ */
+public class Tester {
+}
